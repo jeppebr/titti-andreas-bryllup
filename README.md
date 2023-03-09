455f83f479a3a441a7a0cdeac65a0da53f3e8844
jeppebr <jeppebr95@hotmail.com>
2023-03-09 19:35:04 +0100

    add pwa plugin, not sure cache works

--- README.md
+++ README.md
@@ -1,22 +1,38 @@
+// Sanity, Vite-React, Netlify løsning
+
+// Brugte npm pakker;
 // npm create vite@latest
 // npm i vite-plugin-mkcert -D
 // npm i @vitejs/plugin-react-refresh
 // npm install -D tailwindcss
+// npm i @sanity/client
+// npm i vite-plugin-pwa -D
 
+// Netlify:
+// https://app.netlify.com/sites/melodious-stroopwafel-7633f3/deploys
 
-// npm install @storyblok/react
-// https://app.storyblok.com/#/me/spaces/204571/stories/0/0/273304420
-// virkede ret omstændigt
-
-
-// brug istedet:
+// CMS
 // https://www.sanity.io/pricing
-// https://www.sanity.io/guides/create-a-single-page-application-with-react-and-sanity
-// setup vid for vite, sanity, netlify: https://www.youtube.com/watch?v=jpiNHakNa-4
+// https://www.sanity.io/manage/personal/project/14ve9gqx/api
+// hust at whiteliste din frontend: https://www.sanity.io/manage/personal/project/14ve9gqx/api#cors-origins
 
+// Setup vid for vite, sanity, netlify:
+// https://www.youtube.com/watch?v=jpiNHakNa-4
+// https://www.youtube.com/watch?v=NO7_jgzVgbc
 
+//PWA dokumentation:
+// https://vite-pwa-org.netlify.app/guide/
+// https://www.youtube.com/watch?v=YSGLw4T8BgQ
 
+// Tailwind css
+// https://tailwindcss.com/docs/installation
 
-// how to run:
+// How to run:
 // npm run dev
 // sanity start
+// sanity deploy
+// https://titti-andreas-bryllup.sanity.studio/
+
+// TODO:
+// Opsætning af routes
+// Impl af PWA (ved der findes et vite plugin hertil)
--- vite-react-swc/vite.config.ts
+++ vite-react-swc/vite.config.ts
@@ -1,8 +1,60 @@
-import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
+import { VitePWA } from 'vite-plugin-pwa';
+
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
   server: { https: true },
-  plugins: [mkcert(), reactRefresh()]
-})
+  plugins: [
+    mkcert(),
+    reactRefresh(),
+    VitePWA({
+      injectRegister: 'auto',
+      // registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Titti og andreas bryllup',
+        short_name: 'T+A=<3',
+        description: 'Bryllups info for Titti og Andreas bryllup',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/icons/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        //        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        sourcemap: true,
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith(
+                '/v2021-10-21/data/query/production'
+              );
+            },
+            handler: 'CacheFirst' as const,
+            options: {
+              cacheName: 'api-cache',
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
